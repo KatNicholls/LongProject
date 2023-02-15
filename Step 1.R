c1a2127ec7cc8343ff9295a70d90523ce9e48759
KatNicholls <113170744+KatNicholls@users.noreply.github.com>
2023-02-15 15:03:54 +0000

    Update Step 1.R

--- Step 1.R
+++ Step 1.R
@@ -1,11 +1,15 @@
 
-#STEP 1 GETTING THE LOCATIONS OF TEs from EDTA 
+# 001 Getting TE locations from EDTA file --------------------------------------
+
 library(dplyr)
-raw_files <- data.frame(filename=list.files('~/katherine/005TEs/002EDTA'))
+setwd("/home/irh25/katherine/005TEs/002EDTA")
+raw_files <- data.frame(filename=list.files('~/katherine/005TEs/002EDTA', pattern="gff3"))
 raw_files <- raw_files %>% mutate(name_csv = gsub("\\..*",".csv",raw_files$filename)) 
 
-
 for (i in 1:nrow(raw_files)){
+  
+  setwd("/home/irh25/katherine/005TEs/002EDTA")
+  
   x <- read.delim2(raw_files[i,1], header = FALSE)
   
   #correcting for when chromosome isnt simple Chr1->5 to allow for next step
@@ -15,8 +19,10 @@ for (i in 1:nrow(raw_files)){
   
   TEs <- x %>% filter(V3 %in% c("LTR_retrotransposon", "Copia_LTR_retrotransposon", "Gypsy_LTR_retrotransposon")) %>% select(chrm = V1, start = V4, stop = V5, name = V3)
   
-    write.csv(TEs, raw_files$name_csv[i], quote=FALSE, row.names=FALSE)
+  setwd("/home/irh25/katherine/009REPEAT/001TE_Locations")
   
- }
+  write.csv(TEs, raw_files$name_csv[i], quote=FALSE, row.names=FALSE)
+  
+}
 
 
